5167a6792c92cf3abdbd4c87abaa909fb6936d2e
[Assassin] <ShyAssassin@assassin.dev>
2025-12-30 23:09:36 +0200

    logger: Refactor

--- crates/logger/src/lib.rs
+++ crates/logger/src/lib.rs
@@ -3,18 +3,75 @@ use std::sync::{RwLock, OnceLock};
 use colored::{ColoredString, Colorize};
 use log::{Level, Log, Metadata, Record, SetLoggerError};
 
+#[derive(Debug, Clone)]
+#[derive(PartialEq, Eq, Hash)]
+pub struct LogSource {
+    pub toplevel: String,
+    pub submodules: Vec<String>,
+}
+
+impl LogSource {
+    pub fn new(toplevel: &str, submodules: Vec<&str>) -> Self {
+        return Self {
+            toplevel: toplevel.to_string(),
+            submodules: submodules.iter().map(|&s| s.to_string()).collect(),
+        }
+    }
+
+    pub fn from_target(target: &str) -> Self {
+        let mut parts = target.split("::").into_iter();
+        let toplevel = parts.next().unwrap_or("").to_string();
+        let submodules = parts.map(|sm| String::from(sm)).filter(|sm| {
+            // The last submodule specified is treated as a wildcard
+            // Also nuke relative module path specifiers, stinky!
+            !["*", ":", ".", "/", "\\",].contains(&sm.as_str())
+        }).collect();
+
+        return Self {
+            toplevel: toplevel,
+            submodules: submodules,
+        }
+    }
+
+    pub fn matches(&self, target: &str) -> bool {
+        let source = Self::from_target(target);
+
+        if source.submodules.len() < self.submodules.len() {
+            return false;
+        } else if self.toplevel != source.toplevel  {
+            return false;
+        }
+
+        for (i, submodule) in self.submodules.iter().enumerate() {
+            if submodule != &source.submodules[i] {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    pub fn target(&self) -> String {
+        return format!("{}::{}", self.toplevel, self.submodules.join("::"));
+    }
+}
+
+pub type LogCallSink = Box<dyn Fn(&Record) + Send + Sync>;
+
 pub struct Logger {
     pub log_level: RwLock<Level>,
-    pub crate_levels: RwLock<HashMap<String, Level>>,
+    pub sinks: RwLock<Vec<LogCallSink>>,
+    pub levels: RwLock<HashMap<LogSource, Level>>,
 }
 
 static LOGGER: OnceLock<Logger> = OnceLock::new();
 
 impl Logger {
-    pub fn new(level: Level) -> Logger {
+    pub fn new(level: Level, levels: Vec<(LogSource, Level)>) -> Logger {
         Logger {
             log_level: RwLock::new(level),
-            crate_levels: RwLock::new(HashMap::new()),
+            sinks: RwLock::new(Vec::new()),
+            levels: RwLock::new(levels.into_iter().collect()),
         }
     }
 
@@ -35,18 +92,35 @@ impl Logger {
 
 impl Log for Logger {
     fn enabled(&self, metadata: &Metadata) -> bool {
-        let log_level = *self.log_level.read().unwrap();
-        let crate_levels = self.crate_levels.read().unwrap();
-        let crate_name = metadata.target().split("::").next().unwrap();
+        let target = metadata.target();
+        let sources = self.levels.read().unwrap();
+        let log_level = self.log_level.read().unwrap();
 
-        if let Some(level) = crate_levels.get(crate_name) {
-            return metadata.level() <= *level;
+        let mut best_specificity: i32 = -1;
+        let mut best_match: Option<Level> = None;
+
+        for (source, level) in sources.iter() {
+            if source.matches(target) {
+                let specificity = source.submodules.len() as i32;
+                if specificity > best_specificity {
+                    best_match = Some(*level);
+                    best_specificity = specificity;
+                }
+            }
         }
 
-        return metadata.level() <= log_level;
+        if let Some(level) = best_match {
+            return metadata.level() <= level;
+        } else {
+            return metadata.level() <= *log_level;
+        }
     }
 
     fn log(&self, record: &Record) {
+        for callback in self.sinks.read().unwrap().iter() {
+            callback(record);
+        }
+
         if self.enabled(record.metadata())  {
             #[cfg(not(target_family = "wasm"))]
             eprintln!("{}: {} - {}", self.colorize(record.level()), record.target(), record.args());
@@ -61,17 +135,43 @@ impl Log for Logger {
 
 pub fn init() -> Result<(), SetLoggerError> {
     let logger = LOGGER.get_or_init(|| {
-        let env_level = std::env::var("RUST_LOG").unwrap_or("info".to_string());
-        let level = env_level.parse().unwrap_or(Level::Info);
-        return Logger::new(level);
+        let mut global_level = Level::Info;
+        let mut filters: Vec<(LogSource, Level)> = Vec::new();
+        let env = std::env::var("RUST_LOG").unwrap_or("info".to_string());
+
+        for arg in env.split(",") {
+            let parts: Vec<String> = arg.split('=')
+                .map(|s| s.replace('-', "_"))
+            .collect();
+
+            match parts.as_slice() {
+                [level] => {
+                    if let Ok(level) = level.parse::<Level>() {
+                        global_level = level;
+                    }
+                }
+                [target, level] => {
+                    if let Ok(level) = level.parse::<Level>() {
+                        let source = LogSource::from_target(target);
+                        filters.push((source, level));
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        return Logger::new(global_level, filters);
     });
 
     std::panic::set_hook(Box::new(move |info| {
-        let location = info.location().map_or("Unknown location".to_string(), |p| format!("{}:{}:{}", p.file(), p.line(), p.column()));
+        let location = info.location().map_or("Unknown location".to_string(), |location| {
+            format!("{}:{}:{}", location.file(), location.line(), location.column())
+        });
         let payload = info.payload().downcast_ref::<String>().map(|s| s.clone()).unwrap_or_else(|| {
             info.payload().downcast_ref::<&str>().unwrap_or(&"Unknown Payload").to_string()
         });
-        // This treats newlines as a pseudo "stack trace" for the panic
+
+        // treat newlines as a "stack trace"
         let payload = payload.lines().enumerate()
             .map(|(i, line)| match (i, line.trim().is_empty()) {
                 (_, true) => String::new(),
@@ -92,14 +192,23 @@ pub fn init() -> Result<(), SetLoggerError> {
     return log::set_logger(logger).map(|()| log::set_max_level(log::LevelFilter::Trace));
 }
 
+/// Get a reference to the global logger
+pub fn get_raw_logger() -> &'static Logger {
+    return LOGGER.get().unwrap();
+}
+
+/// Register a new log sink callback
+pub fn add_sink(callback: LogCallSink) {
+    LOGGER.get().unwrap().sinks.write().unwrap().push(callback);
+}
+
+/// Set the global log level
 pub fn set_level(level: Level) {
     LOGGER.get().unwrap().set_level(level);
 }
 
-pub fn set_crate_log(target: &str, level: Level) {
-    LOGGER.get().unwrap().crate_levels.write().unwrap().insert(target.to_string(), level);
-}
-
-pub fn get_raw_logger() -> &'static Logger {
-    return LOGGER.get().unwrap();
+/// Set the log level for a specific source
+pub fn set_source_log(source: LogSource, level: Level) {
+    // FIXME: account for existing sources!!!!!!
+    LOGGER.get().unwrap().levels.write().unwrap().insert(source, level);
 }
