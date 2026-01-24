85473ba221f182a497cda8d199412a92e4635022
[Assassin] <ShyAssassin@assassin.dev>
2026-01-24 18:47:01 +0200

    gfx: Sampler impl

--- crates/gfx/src/sampler.rs
+++ crates/gfx/src/sampler.rs
@@ -1,67 +1,57 @@
-use crate::Device;
-
-// TODO: consider moving this somewhere else
-pub type FilterMode = wgpu::FilterMode;
-pub type AddressMode = wgpu::AddressMode;
+use gfx_macros::arcanize;
+use crate::{Device, WgpuExt};
 
 #[derive(Debug, Clone, Copy)]
 #[derive(Hash, Eq, PartialEq)]
 pub struct SamplerMode {
-    pub address: AddressMode,
-    pub mag_filter: FilterMode,
-    pub min_filter: FilterMode,
-    pub mip_filter: FilterMode,
+    pub address: wgpu::AddressMode,
+    pub mag_filter: wgpu::FilterMode,
+    pub min_filter: wgpu::FilterMode,
+    pub mip_filter: wgpu::FilterMode,
 }
 
 impl SamplerMode {
     pub const REPEAT: Self = Self {
-        address: AddressMode::Repeat,
-        mag_filter: FilterMode::Linear,
-        min_filter: FilterMode::Nearest,
-        mip_filter: FilterMode::Nearest,
+        address: wgpu::AddressMode::Repeat,
+        mag_filter: wgpu::FilterMode::Linear,
+        min_filter: wgpu::FilterMode::Nearest,
+        mip_filter: wgpu::FilterMode::Nearest,
     };
 
     pub const CLAMP: Self = Self {
-        address: AddressMode::ClampToEdge,
-        mag_filter: FilterMode::Linear,
-        min_filter: FilterMode::Nearest,
-        mip_filter: FilterMode::Nearest,
+        address: wgpu::AddressMode::ClampToEdge,
+        mag_filter: wgpu::FilterMode::Linear,
+        min_filter: wgpu::FilterMode::Nearest,
+        mip_filter: wgpu::FilterMode::Nearest,
     };
 }
 
-impl From<SamplerMode> for wgpu::SamplerDescriptor<'_> {
-    fn from(mode: SamplerMode) -> Self {
-        return Self {
-            label: None,
-            min_filter: mode.min_filter.into(),
-            mag_filter: mode.mag_filter.into(),
-            mipmap_filter: mode.mip_filter.into(),
-            address_mode_u: mode.address.into(),
-            address_mode_v: mode.address.into(),
-            address_mode_w: mode.address.into(),
-            ..Default::default()
-        };
-    }
-}
-
+#[arcanize]
 pub struct Sampler {
+    pub device: Device,
     pub mode: SamplerMode,
-    pub name: Option<String>,
+    pub label: Option<String>,
     pub sampler: wgpu::Sampler,
 }
 
 impl Sampler {
-    pub fn new(device: &Device, name: Option<String>, mode: SamplerMode) -> Self {
-        let name = name.unwrap_or("Unnamed Sampler".into());
-        todo!()
-    }
-}
+    pub fn new(device: &Device, label: Option<String>, mode: SamplerMode) -> Self {
+        let sampler = device.wgpu_create_sampler(&wgpu::SamplerDescriptor {
+            label: label.as_deref(),
+            min_filter: mode.min_filter,
+            mag_filter: mode.mag_filter,
+            address_mode_u: mode.address,
+            address_mode_v: mode.address,
+            address_mode_w: mode.address,
+            mipmap_filter: mode.mip_filter,
+            ..Default::default()
+        });
 
-impl std::fmt::Debug for Sampler {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Sampler")
-            .field("name", &self.name)
-            .field("sampler", &(&self.sampler as *const _))
-        .finish()
+        return SamplerInner {
+            mode: mode,
+            label: label,
+            sampler: sampler,
+            device: device.clone(),
+        }.into();
     }
 }
