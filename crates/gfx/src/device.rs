ef1ca49102f99c81bb48359cda5cde04b0d7d6ee
[Assassin] <ShyAssassin@assassin.dev>
2026-06-15 21:27:17 +0200

    gfx: Add features on device init

--- crates/gfx/src/device.rs
+++ crates/gfx/src/device.rs
@@ -7,20 +7,29 @@ pub struct Device {
 }
 
 impl Device {
-    // TODO: consider just blocking on this function
+    const WANTED_FEATURES: &[wgpu::Features] = &[
+        wgpu::Features::TIMESTAMP_QUERY,
+        wgpu::Features::TIMESTAMP_QUERY_INSIDE_PASSES,
+        wgpu::Features::TEXTURE_ADAPTER_SPECIFIC_FORMAT_FEATURES,
+    ];
+
     pub async fn new() -> Self {
         let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-            backends: wgpu::Backends::PRIMARY,
-            flags: wgpu::InstanceFlags::from_build_config(),
+            backends: wgpu::Backends::PRIMARY.with_env(),
+            flags: wgpu::InstanceFlags::from_env_or_default(),
+            memory_budget_thresholds: wgpu::MemoryBudgetThresholds::default(),
             backend_options: wgpu::BackendOptions {
-                gl: wgpu::GlBackendOptions::default(),
+                gl: wgpu::GlBackendOptions::from_env_or_default(),
+                noop: wgpu::NoopBackendOptions::from_env_or_default(),
                 dx12: wgpu::Dx12BackendOptions {
                     shader_compiler: wgpu::Dx12Compiler::DynamicDxc {
-                        dxil_path: "bin/".into(),
-                        dxc_path: "bin/".into()
-                    }
-                },
-            }
+                        dxc_path: "bin/dxcompiler.dll".into(),
+                        max_shader_model: wgpu::DxcShaderModel::V6_5,
+                    },
+                    presentation_system: wgpu::wgt::Dx12SwapchainKind::DxgiFromHwnd,
+                    latency_waitable_object: wgpu::wgt::Dx12UseFrameLatencyWaitableObject::Wait,
+                }.with_env(),
+            },
         });
 
         let adapter = instance.request_adapter(&wgpu::RequestAdapterOptionsBase {
@@ -28,13 +37,28 @@ impl Device {
             force_fallback_adapter: false,
             power_preference: wgpu::PowerPreference::HighPerformance
         }).await.expect("Failed to get an adapter");
+        let info = adapter.get_info();
+        log::info!("Using {:?} {} ({:?})", info.device_type, info.name, info.backend);
+
+        let adapter_features = adapter.features();
+        let mut supported_features = wgpu::Features::empty();
+        for feature in Self::WANTED_FEATURES {
+            if adapter_features.contains(*feature) {
+                supported_features |= *feature;
+            } else {
+                log::warn!("{} is not supported by adapter", feature);
+            }
+        }
 
         let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
-            label: Some("gfx Device"),
+            trace: wgpu::Trace::Off,
+            label: Some("Pregen GFX Device"),
+            required_features: supported_features,
             required_limits: wgpu::Limits::default(),
-            required_features: wgpu::Features::empty(),
-            memory_hints: wgpu::MemoryHints::Performance,
-        }, None).await.expect("Failed to get a device");
+            memory_hints: wgpu::MemoryHints::default(),
+            experimental_features: wgpu::ExperimentalFeatures::disabled(),
+        }).await.expect("Failed to acquire a suitable device");
+        log::info!("Enabled features: [{}]", device.features());
 
         return Self {
             queue: queue,
